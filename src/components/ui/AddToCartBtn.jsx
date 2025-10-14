53b2eef57f91778228d5ad77dfad6f6dbff6e0e0
Rijul Sharma <srijul80@gmail.com>
2025-10-14 16:30:01 +0530

    worked on add to cart functionality and add to cart items

--- src/components/ui/AddToCartBtn.jsx
+++ src/components/ui/AddToCartBtn.jsx
@@ -1,11 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { CartContext } from "../../Context/Cart/CartContext";
 
 const AddToCartBtn = ({ product, quantity }) => {
   const { cartItems, setCartItems } = useContext(CartContext);
+  const handleAddToCart = () => {
+    const newItem = { product, quantity };
 
-  const handleAddToCart = () => {};
+    // Check if product already exists
+    const exists = cartItems.some(
+      (item) => item.product.id === newItem.product.id,
+    );
+
+    if (exists) {
+      alert("Already added to cart");
+      return;
+    }
+
+    setCartItems((prev) => [...prev, newItem]);
+  };
+
+  useEffect(() => {
+    console.log("Cart updated:", cartItems);
+  }, [cartItems]);
+
   return (
     <button
       className="flex w-full items-center justify-center gap-2 rounded-lg bg-indigo-600 px-4 py-2 font-medium text-white transition duration-300 hover:bg-indigo-700 hover:shadow-md active:scale-95"
